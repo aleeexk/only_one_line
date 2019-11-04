1cb05faad7828e711b190e27dff7308d126ff037
aleeexk <53943917+aleeexk@users.noreply.github.com>
2019-11-04 23:30:23 +0700

    Add files via upload

---
+++ only_one_line.py
@@ -0,0 +1,8 @@
+import math
+lst = [1, 8, 7, 9, 2, 5, 7, 7, 9, 3, 4]
+
+print(math.factorial(1000))
+print(sum(range(1, 2 ** 25)))
+print([j + str(i) for i in range(1, 11) for j in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']])
+print([i * 2 if i % 2 else i for i in lst])
+print(str(math.pi)[:22])
